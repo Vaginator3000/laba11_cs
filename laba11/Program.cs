8376d04f32b2bc150141463fb328d03d42b51500
Vladimir <Shifman.1704@mail.ru>
2020-12-24 20:09:20 +0700

    Добавьте файлы проекта.

---
+++ laba11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Одномерный массив");
+
+            Auto[] cars1 = new Auto[2] {
+               new Auto("car0", 400, 300000),
+               new Auto("car1", 430, 333320),
+            };
+
+            for (int i = 0; i < 2; i++) 
+                cars1[i].display();
+
+
+            Console.WriteLine("\nДвумерный массив");
+
+            Auto[][] cars2 = new Auto[2][];
+
+            for (int i = 0; i < 2; i++)
+                cars2[i] = new Auto[2];
+
+            for (int i = 0; i < 2; i++) 
+                for (int j = 0; j < 2; j++)
+                    cars2[i][j] = new Auto("car" + i.ToString() + j.ToString(), 7*(i+j+1), 20*(i+j+1));
+
+            for (int i = 0; i < 2; i++) 
+                for (int j = 0; j < 2; j++)
+                    cars2[i][j].display();
+        }
+
+    }
+}
